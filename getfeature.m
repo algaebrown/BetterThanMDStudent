ee501597e2b52634651edb83e8cacd765644d97a
b101102109@tmu.edu.tw <b101102109@tmu.edu.tw>
2016-08-20 10:38:17 +0800

    change to relative directory

--- getfeature.m
+++ getfeature.m
@@ -19,7 +19,7 @@ load('Springer_total_obs_distribution.mat');
 springer_options   = default_Springer_HSMM_options;
 cd(['../training/training-',recordName(1)]);
 [PCG, Fs1, nbits1] = wavread([recordName '.wav']);  % load data
-cd('C:/Users/Administrator/Google Drive/heartSound/sample2016');
+cd('../../sample2016');
 PCG_resampled      = resample(PCG,springer_options.audio_Fs,Fs1); % resample to springer_options.audio_Fs (1000 Hz)
 
 %% Running runSpringerSegmentationAlgorithm.m to obtain the assigned_states
